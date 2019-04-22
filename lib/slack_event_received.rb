cac6dee60f3ec0664e6accddc03abac3c8d54c59
Joshua Flanagan <joshuaflanagan@gmail.com>
2019-04-21 22:00:08 -0500

    Refactor SlackEventReceived

--- lib/slack_event_received.rb
+++ lib/slack_event_received.rb
@@ -22,33 +22,41 @@ class SlackEventReceived
     payload = JSON.parse(event["body"])
 
     slack_message_type = payload["type"]
-    # temporary, so slack doesnt disable our app
-    if slack_message_type == "event_callback"
-      slack_event = payload.fetch("event")
-      slack_event_type = slack_event.fetch("type")
-      if slack_event_type == "link_shared"
-        first_github_link = slack_event["links"].detect{|link|
-          link["domain"] == "github.com"
-        }
-        if first_github_link
-          url = first_github_link.fetch("url")
-          pr_id = url.split(/github\.com\//, 2).last
-          mention_id = slack_event.fetch("message_ts")
-          mentions_store.save(pr_id: pr_id, mention_id: mention_id)
-        end
-      else
-        logger << "UNHANDLED EVENT CALLBACK: #{event}"
-      end
-      return success_response({ok: true})
-    end
-    unless slack_message_type == "url_verification"
+
+    case slack_message_type
+    when "url_verification"
+      url_verify(payload)
+    when "event_callback"
+      slack_event(payload)
+    else
       logger << "UNRECOGNIZED TYPE: #{event}"
       return fail_response(400)
     end
+  end
 
+  def slack_event(payload)
+    slack_event = payload.fetch("event")
+    slack_event_type = slack_event.fetch("type")
+    if slack_event_type == "link_shared"
+      first_github_link = slack_event["links"].detect{|link|
+        link["domain"] == "github.com"
+      }
+      if first_github_link
+        url = first_github_link.fetch("url")
+        pr_id = url.split(/github\.com\//, 2).last
+        mention_id = slack_event.fetch("message_ts")
+        mentions_store.save(pr_id: pr_id, mention_id: mention_id)
+      end
+    else
+      logger << "UNHANDLED EVENT CALLBACK: #{payload}"
+    end
+    return success_response({ok: true})
+  end
+
+  def url_verify(payload)
     challenge = payload["challenge"]
     unless challenge
-      logger << "NO CHALLENGE: #{event}"
+      logger << "NO CHALLENGE: #{payload}"
       return fail_response(400)
     end
 
